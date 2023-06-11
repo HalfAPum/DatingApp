f9c8c3f5c48b677a6654216d5ba649144751d8af
Oleksandr Narvatov <hipi96222@gmail.com>
2023-06-11 12:51:02 +0300

    Improve user input user experience

--- app/src/main/java/com/narvatov/datingapp/ui/screen/sign/SignIn.kt
+++ app/src/main/java/com/narvatov/datingapp/ui/screen/sign/SignIn.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.OutlinedTextField
@@ -17,7 +18,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -50,6 +54,8 @@ fun SignIn(
             modifier = Modifier.fillMaxWidth()
         )
 
+        val focusManager = LocalFocusManager.current
+
         var email by rememberSaveable { mutableStateOf("") }
 
         OutlinedTextField(
@@ -58,7 +64,13 @@ fun SignIn(
             label = {
                 Text(text = stringResource(R.string.email))
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
             singleLine = true,
             modifier = Modifier.padding(top = 40.dp).fillMaxWidth()
         )
@@ -71,7 +83,14 @@ fun SignIn(
             label = {
                 Text(text = stringResource(R.string.password))
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = {
+                viewModel.signIn(email, password)
+                focusManager.clearFocus()
+            }),
             singleLine = true,
             modifier = Modifier.padding(top = 20.dp).fillMaxWidth()
         )
@@ -81,6 +100,7 @@ fun SignIn(
             modifier = Modifier.padding(top = 40.dp)
         ) {
             viewModel.signIn(email, password)
+            focusManager.clearFocus()
         }
 
         WideButton(
--- app/src/main/java/com/narvatov/datingapp/ui/screen/sign/SignUp.kt
+++ app/src/main/java/com/narvatov/datingapp/ui/screen/sign/SignUp.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.OutlinedTextField
@@ -19,7 +20,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -62,6 +66,8 @@ fun SignUp(
                 .align(Alignment.CenterHorizontally),
         )
 
+        val focusManager = LocalFocusManager.current
+
         var firstName by rememberSaveable { mutableStateOf("") }
 
         OutlinedTextField(
@@ -70,7 +76,13 @@ fun SignUp(
             label = {
                 Text(text = stringResource(R.string.first_name))
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
             singleLine = true,
             modifier = Modifier.padding(top = 20.dp).fillMaxWidth()
         )
@@ -83,7 +95,13 @@ fun SignUp(
             label = {
                 Text(text = stringResource(R.string.last_name))
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
             singleLine = true,
             modifier = Modifier.padding(top = 10.dp).fillMaxWidth()
         )
@@ -96,7 +114,13 @@ fun SignUp(
             label = {
                 Text(text = stringResource(R.string.email))
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
             singleLine = true,
             modifier = Modifier.padding(top = 10.dp).fillMaxWidth()
         )
@@ -109,7 +133,14 @@ fun SignUp(
             label = {
                 Text(text = stringResource(R.string.password))
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = {
+                viewModel.signUp(email, password, firstName, lastName, photoBitmap)
+                focusManager.clearFocus()
+            }),
             singleLine = true,
             modifier = Modifier.padding(top = 10.dp).fillMaxWidth()
         )
@@ -117,7 +148,10 @@ fun SignUp(
         WideButton(
             text = stringResource(R.string.sign_up),
             modifier = Modifier.padding(top = 20.dp)
-        ) { viewModel.signUp(email, password, firstName, lastName, photoBitmap) }
+        ) {
+            viewModel.signUp(email, password, firstName, lastName, photoBitmap)
+            focusManager.clearFocus()
+        }
 
         ErrorText(
             errorViewModel = viewModel,
